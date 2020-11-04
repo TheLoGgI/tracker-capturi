342e02b1bccdfdb80a460ee1e4bb1b14375f855d
Lasse Aakjær <51378679+TheLoGgI@users.noreply.github.com>
2020-11-04 15:00:17 +0100

    Code commens

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 let gen = idMaker() // Id generator function from developer.mozilla
 
-const trackerList = []
+
+const trackerList = [] // Used for creating the current tracker
+
+// Start inventory
 let trackedList = [
     {
         id: gen.next().value,
@@ -19,12 +22,14 @@ let trackedList = [
     },
     ]
 
+    // Display trackers on load, and add eventhandlers to the remove buttons
 document.addEventListener('DOMContentLoaded', e => {
     displayList(trackedList)
     removeHandlerEvent()
 })
 
 
+// Create tracker and reset values on submit 
 document.getElementById('formtracker').addEventListener('submit', e => {
     e.preventDefault()
 
@@ -39,7 +44,7 @@ document.getElementById('formtracker').addEventListener('submit', e => {
     removeHandlerEvent()
 })
 
-// Add tracker words to tracker
+// Add "tracker words" to tracker
 document.getElementById('wordTrackerInput').addEventListener('click', e => {
     const addTrackWord = document.getElementById('wordtracker').value
     trackerList.push(addTrackWord)
@@ -54,7 +59,6 @@ document.getElementById('wordTrackerInput').addEventListener('click', e => {
 document.getElementById('filterTrackers').addEventListener('keyup', e=> {
 
     let filterdArray = []
-
 
     if (e.target.value !== '') {
         if (isNaN(e.target.value)) {
@@ -76,14 +80,14 @@ document.getElementById('filterTrackers').addEventListener('keyup', e=> {
     
 })
 
-
+// Display function for tracker list items
 function displayList(list) {
     let htmlstring = ''
     for (const prop of list) {
         htmlstring += `
         <div class="tracker-item" data-id="${prop.id}">
             <p>${prop.name}</p>
-            <p class="tracker-item-list">${stringList(prop.trackerList)}</p>
+            <p class="tracker-item-list">${dashString(prop.trackerList)}</p>
             <svg class="remove" viewBox="0 0 365.696 365.696"><path d="m243.1875 182.859375 113.132812-113.132813c12.5-12.5 12.5-32.765624 0-45.246093l-15.082031-15.082031c-12.503906-12.503907-32.769531-12.503907-45.25 0l-113.128906 113.128906-113.132813-113.152344c-12.5-12.5-32.765624-12.5-45.246093 0l-15.105469 15.082031c-12.5 12.503907-12.5 32.769531 0 45.25l113.152344 113.152344-113.128906 113.128906c-12.503907 12.503907-12.503907 32.769531 0 45.25l15.082031 15.082031c12.5 12.5 32.765625 12.5 45.246093 0l113.132813-113.132812 113.128906 113.132812c12.503907 12.5 32.769531 12.5 45.25 0l15.082031-15.082031c12.5-12.503906 12.5-32.769531 0-45.25zm0 0"/></svg>
         </div>` 
         
@@ -93,7 +97,7 @@ function displayList(list) {
     removeHandlerEvent()
 }
 
-
+// Function for creating eventhandlers for every remove button
 function removeHandlerEvent() {
     const removeList = document.querySelectorAll('.remove')
     for (const remover of removeList) {
@@ -101,16 +105,14 @@ function removeHandlerEvent() {
     }
 }
 
-
+// Removes tracker from DOM and trackedList + rerendner
 function removeTracker(e) {
     // console.dir(e.target);
     let elementId = null
     if (e.target.tagName === 'svg') {
-        // console.log(e.target.parentNode.dataset.id);
         elementId = e.target.parentNode.dataset.id
     } else if (e.target.tagName === 'path') {
         elementId = e.target.parentNode.parentNode.dataset.id
-        // console.dir(e.target.parentNode.parentNode.dataset.id);
     }
 
     trackedList.forEach((item, index) => {
@@ -122,6 +124,7 @@ function removeTracker(e) {
     displayList(trackedList)
 }
 
+// Creates a tracker object in trackedList 
 function createTracker(name, trackerList) {
     trackedList.push({
         id: gen.next().value,
@@ -129,8 +132,8 @@ function createTracker(name, trackerList) {
         trackerList
     })
 }
-
-function stringList(array) {
+// Creates string with (-)  
+function dashString(array) {
     return array.toString().split(',').join(' - ')
 }
 
